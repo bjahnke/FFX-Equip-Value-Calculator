f48c45aeed7bc0ef2fe162cb9bf52410837842b9
Brian Jahnke <bjahnke71@gmail.com>
2019-02-08 16:33:59 -0500

    Ability Data copies to table

--- src/pkg1/WeamorValueCalc.java
+++ src/pkg1/WeamorValueCalc.java
@@ -21,37 +21,44 @@ public class WeamorValueCalc {
 	private static Hashtable<String, AbilityData> abilityTable = new Hashtable<String, AbilityData>();
 	
 	public static void main(String[] args) throws IOException {
-		
+		abilityTable = parseText();
 	}
 	
 	public static Hashtable<String, AbilityData> parseText() throws IOException{
 		Hashtable<String, AbilityData> aTable = new Hashtable<String, AbilityData>();
 		BufferedReader br = new BufferedReader(new FileReader("abilityList.txt"));
 		String aDataStr = "";
-		Pattern p_gil = Pattern.compile("^\\d+");
+		Pattern p_gil = Pattern.compile("^[\\d]+");
 		Pattern p_aName = Pattern.compile("(?<=[-]+)[A-Za-z ]+(?=[:])");
-		Pattern p_rName = Pattern.compile("(?<=(: *))[A-Za-z ]+(?=( x))");
+		Pattern p_rName = Pattern.compile("(?<=(: ))[A-Za-z ]+(?=( x))");
 		Pattern p_rQ = Pattern.compile("\\d+$");
 		
-		Pattern isData = p_gil;     //the line is data if it starts with a digit
+		Pattern isData = p_gil;
 		
 		while((aDataStr = br.readLine()) != null){
 			Matcher dataMatch = isData.matcher(aDataStr);
 			
 			//if the line is data then we pattern match and store
-			if(dataMatch.matches()){
+			if(dataMatch.find()){
 				Matcher m_gil = p_gil.matcher(aDataStr);
 				Matcher m_aName = p_aName.matcher(aDataStr);
 				Matcher m_rName = p_rName.matcher(aDataStr);
 				Matcher m_rQ = p_rQ.matcher(aDataStr);
 				
-				String gil = m_gil.group();
-				String aName = m_aName.group();
-				String rName = m_rName.group();
-				String rQ = m_rQ.group();
-				
-				AbilityData abilityData = new AbilityData(gil, aName, rName, rQ);
-				aTable.put(aName, abilityData);
+				if(m_gil.find()){
+					String gil = m_gil.group();
+					if(m_aName.find()){
+						String aName = m_aName.group();
+						if(m_rName.find()){
+							String rName = m_rName.group();
+							if(m_rQ.find()){
+								String rQ = m_rQ.group();
+								AbilityData abilityData = new AbilityData(gil, aName, rName, rQ);
+								aTable.put(aName, abilityData);
+							}
+						}
+					}
+				}
 			}
 		}
 		System.out.println(aTable.toString());
